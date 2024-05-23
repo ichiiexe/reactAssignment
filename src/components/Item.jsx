252fbba1f487980cf2b53eb9e99db2fd65105bf8
Jefferson <jeffabuy@gmail.com>
2024-05-23 07:50:22 +0300

    added outline on InputContainers

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -19,10 +19,21 @@ export function Section(props) {
   return (
     <>
       <li className="px-4 flex flex-col gap-3">
-        <div className="bg-blue-600 text-white p-4 rounded-lg w-full after:text-white flex justify-between">
+        <div
+          onClick={toggleDropDown}
+          className={`bg-blue-600 text-white p-4 rounded-lg w-full after:text-white flex justify-between ${
+            !dropDown ? "after:content-['+']" : "after:content-['-']"
+          }`}
+        >
           {props.title}
         </div>
-        <div className="w-full p-4 bg-white rounded-md">{ListItem}</div>
+        <div
+          className={`w-full p-4 bg-white rounded-md border border-black/15 ${
+            !dropDown ? "hidden" : ""
+          }`}
+        >
+          {ListItem}
+        </div>
       </li>
       ;
     </>
