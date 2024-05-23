6dcda03b97560719e1fb1c8595ef07d5465c665f
Jefferson <jeffabuy@gmail.com>
2024-05-23 07:09:40 +0300

    added DIV on ul

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-function App() {
-  const [count, setCount] = useState(0);
+import { Section } from "./components/Item";
 
+function App() {
   return (
     <>
-      <ul className="flex flex-col min-w-96 w-3/5 rounded-lg bg-gray-700">
-        <h1 className="m-6 font-bold text-3xl text-center">Expandable Form</h1>
-        <Section title="Section 1" text={["Name:", "Email:"]} />
-        <Section title="Section 2" text={["Address:", "City"]} />
-        <Section title="Section 3" text={["Phone", "Website"]} />
-      </ul>
+      <div className="flex justify-center items-center h-dvh bg-slate-500">
+        <ul className="flex flex-col min-w-96 w-3/5 rounded-lg bg-gray-700">
+          <h1 className="m-6 font-bold text-3xl text-center">
+            Expandable Form
+          </h1>
+          <Section title="Section 1" text={["Name:", "Email:"]} />
+          <Section title="Section 2" text={["Address:", "City"]} />
+          <Section title="Section 3" text={["Phone", "Website"]} />
+        </ul>
+      </div>
     </>
   );
 }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,8 +3,15 @@ import { useState } from "react";
 export function Section(props) {
   const [dropDown, setDropDown] = useState(false);
 
-  <li className="p-4 flex flex-col gap-3">
-    <div className="bg-blue-600 text-white p-4 rounded-lg w-full after:text-white flex justify-between"></div>
-    <div></div>
-  </li>;
+  return (
+    <>
+      <li className="p-4 flex flex-col gap-3">
+        <div className="bg-blue-600 text-white p-4 rounded-lg w-full after:text-white flex justify-between">
+          {props.title}
+        </div>
+        <div></div>
+      </li>
+      ;
+    </>
+  );
 }
