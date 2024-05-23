d8d1933183aac81a40d54bd95ae042c891be8987
Jefferson <jeffabuy@gmail.com>
2024-05-23 07:07:34 +0300

    added SectionTitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul className="flex flex-col min-w-96 w-3/5 rounded-lg bg-gray-700">
+        <h1 className="m-6 font-bold text-3xl text-center">Expandable Form</h1>
+        <Section title="Section 1" text={["Name:", "Email:"]} />
+        <Section title="Section 2" text={["Address:", "City"]} />
+        <Section title="Section 3" text={["Phone", "Website"]} />
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Item.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export function Section(props) {
+  const [dropDown, setDropDown] = useState(false);
+
+  <li className="p-4 flex flex-col gap-3">
+    <div className="bg-blue-600 text-white p-4 rounded-lg w-full after:text-white flex justify-between"></div>
+    <div></div>
+  </li>;
+}
