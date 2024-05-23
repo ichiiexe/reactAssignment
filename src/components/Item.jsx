f9ddc62b097fc0a46ebdfbbfbc52b1dd571f9677
Jefferson <jeffabuy@gmail.com>
2024-05-23 07:32:06 +0300

    added SelectionItems

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,6 +2,19 @@ import { useState } from "react";
 
 export function Section(props) {
   const [dropDown, setDropDown] = useState(false);
+  const toggleDropDown = () => {
+    setDropDown(!dropDown);
+  };
+  const inputItem = props.text;
+  const ListItem = inputItem.map((item, index) => (
+    <div className="m-2" key={index}>
+      <h1>{item}</h1>
+      <input
+        className="w-full border rounded-md p-2 focus:outline-blue-600"
+        type="text"
+      />
+    </div>
+  ));
 
   return (
     <>
@@ -9,7 +22,7 @@ export function Section(props) {
         <div className="bg-blue-600 text-white p-4 rounded-lg w-full after:text-white flex justify-between">
           {props.title}
         </div>
-        <div></div>
+        <div>{ListItem}</div>
       </li>
       ;
     </>
