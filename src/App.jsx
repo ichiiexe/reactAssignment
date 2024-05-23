d129092352ec20d0422f00458f28b089d937bd09
Jefferson <jeffabuy@gmail.com>
2024-05-23 07:54:22 +0300

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ function App() {
   return (
     <>
       <div className="flex justify-center items-center h-dvh">
-        <ul className="flex flex-col min-w-96 w-3/5 rounded-lg bg-black/5">
-          <h1 className="m-6 font-bold text-3xl text-center">
+        <ul className="flex flex-col gap-4 py-8 min-w-96 w-3/5 rounded-lg bg-black/5">
+          <h1 className="mb-6 font-bold text-3xl text-center">
             Expandable Form
           </h1>
           <Section title="Section 1" text={["Name:", "Email:"]} />
-          <Section title="Section 2" text={["Address:", "City"]} />
-          <Section title="Section 3" text={["Phone", "Website"]} />
+          <Section title="Section 2" text={["Address:", "City:"]} />
+          <Section title="Section 3" text={["Phone:", "Website:"]} />
         </ul>
       </div>
     </>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -35,7 +35,6 @@ export function Section(props) {
           {ListItem}
         </div>
       </li>
-      ;
     </>
   );
 }
